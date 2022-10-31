3df6cf195c652cc0838eb060df0dcc886e6ec19b
Dmytro Dovbii <dmytro.d@quali.com>
2022-10-31 16:08:29 +0200

    disable version check

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,10 +22,10 @@ fi
 
 echo "The following parameters will be used: ${params}"
 
-sandbox_id=$(torque sb start ${params} --output=json | tr -d '"') || exit 1
+sandbox_id=$(torque --disable-version-check sb start ${params} --output=json | tr -d '"') || exit 1
 echo "Started sandbox with id '${sandbox_id}'"
 
-sandbox_details=$(torque sb get ${sandbox_id} --output=json --detail) || exit 1
+sandbox_details=$(torque --disable-version-check sb get ${sandbox_id} --output=json --detail) || exit 1
 
-echo "sandbox_id=$sandbox_id" >> $GITHUB_OUTPUT
-echo "sandbox_details=$sandbox_details" >> $GITHUB_OUTPUT
+echo "sandbox_id=${sandbox_id}" >> $GITHUB_OUTPUT
+echo "sandbox_details=${sandbox_details}" >> $GITHUB_OUTPUT
