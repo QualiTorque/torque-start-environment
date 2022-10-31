c9e891a18878a2392f8885172743de33c54230f4
Dmytro Dovbii <dmytro.d@quali.com>
2022-10-31 15:32:55 +0200

    save sb details

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,18 +10,22 @@ INPUTS=$6
 echo "Running torque start sandbox command"
 params="${BP_NAME} -n ${SB_NAME} -b ${BRANCH} -d ${DURATION}"
 
-env
-
 if [ "$TIMEOUT" -gt 0 ]; then
     params="$params -w -t ${TIMEOUT}"
 fi
 if [ ! -z ${INPUTS} ]; then
     params="$params -i ${INPUTS}"
 fi
+if [ ! -z ${BRANCH} ]; then
+    params="$params -b ${BRANCH}"
+fi
 
 echo "The following parameters will be used: ${params}"
 
-torque sb start ${params} # --output=json
+sandbox_id=$(torque sb start ${params} --output=json | tr -d '"') || exit 1
+echo "Started sandbox with id '${sandbox_id}'"
 
-# echo "Started sandbox with id '${sandbox_id}'"
-# echo "sandbox_id=$sandbox_id" >> $GITHUB_OUTPUT
+sandbox_details=$(torque sb get ${sandbox_id} --output=json --detail) || exit 1
+
+echo "sandbox_id=$sandbox_id" >> $GITHUB_OUTPUT
+echo "sandbox_details=$sandbox_details" >> $GITHUB_OUTPUT
