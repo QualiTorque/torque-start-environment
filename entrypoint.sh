de8a2cc0ffa2b887492ca1a9cac6fe8ea73811ae
Dmytro Dovbii <dmytro.d@quali.com>
2022-11-09 19:16:34 +0200

    sandbox -> environment

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ DURATION=$4
 TIMEOUT=$5
 INPUTS=$6
 
-echo "Running torque start sandbox command"
+echo "Running torque start environment command"
 params="${BP_NAME} -n ${SB_NAME} -d ${DURATION}"
 
 if [ "$TIMEOUT" -gt 0 ]; then
@@ -22,12 +22,12 @@ fi
 
 echo "The following parameters will be used: ${params}"
 
-echo "Starting the sandbox..."
-sandbox_id=$(torque --disable-version-check sb start ${params} --output=json | tr -d '"') || exit 1
-echo "Started sandbox with id '${sandbox_id}'"
+echo "Starting the environment..."
+environment_id=$(torque --disable-version-check sb start ${params} --output=json | tr -d '"') || exit 1
+echo "Started environment with id '${environment_id}'"
 
-sandbox_details=$(torque --disable-version-check sb get ${sandbox_id} --output=json --detail | tr -d "\n") || exit 1
+environment_details=$(torque --disable-version-check sb get ${environment_id} --output=json --detail | tr -d "\n") || exit 1
 
 echo "Writing data to outputs"
-echo "sandbox_id=${sandbox_id}" >> $GITHUB_OUTPUT
-echo "sandbox_details=${sandbox_details}" >> $GITHUB_OUTPUT
+echo "environment_id=${environment_id}" >> $GITHUB_OUTPUT
+echo "environment_details=${environment_details}" >> $GITHUB_OUTPUT
